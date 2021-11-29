4c0c49c591ba7272a811e15d11c981a0d229f54f
felipevillajr <felipevilla2010@live.com>
2021-11-29 15:45:15 -0500

    added header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Header.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Dropdown from "react-bootstrap/Dropdown";
+
+export default function Header() {
+  return (
+    <section className="Header">
+      <div>TEST BUSINESS LOGO</div>
+      <h1>TEST BUSINESS</h1>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic-button">
+          MENU
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">About</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Contact-Us</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Locations</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </section>
+  );
+}
